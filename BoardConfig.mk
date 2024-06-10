8a03c504c2334ab0aaf25e4bc576b2b2ec229151
Marc Bourgoin <themard85@gmail.com>
2024-06-09 23:23:17 -0600

    dubai: Update blobs from from dubai_g-user 13 T1RDS33.116-33-15-6 e089e7-b95537 release-keys - RETAIL variant
    Change-Id: Id1418267fb9e0dcbddb9efe57dc66657675cec23

---
+++ BoardConfig.mk
@@ -0,0 +1,59 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+DEVICE_PATH := device/motorola/dubai
+
+# Inherit from motorola sm7325-common
+include device/motorola/sm7325-common/BoardConfigCommon.mk
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := dubai
+
+# Fingerprint
+TARGET_SURFACEFLINGER_UDFPS_LIB := //$(DEVICE_PATH):libudfps_extension.dubai
+SOONG_CONFIG_qtidisplay_udfps := true
+
+# HIDL
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(DEVICE_PATH)/device_framework_matrix_dubai.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest_dubai.xml
+
+# Kernel
+BOARD_KERNEL_CMDLINE += androidboot.hab.product=dubai
+TARGET_KERNEL_CONFIG += vendor/lineage_dubai.config
+
+# Kernel Modules
+BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load))
+BOARD_VENDOR_KERNEL_MODULES_BLOCKLIST_FILE := $(DEVICE_PATH)/modules.blocklist
+BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load.recovery))
+BOOT_KERNEL_MODULES := $(BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD)
+
+# Partitions
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 116340535296
+BOARD_MOT_DP_GROUP_SIZE := 8921284608 # ( BOARD_SUPER_PARTITION_SIZE - 4MB )
+BOARD_SUPER_PARTITION_SIZE := 8925478912
+
+# Properties
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+
+# Recovery
+TARGET_RECOVERY_UI_MARGIN_HEIGHT := 90
+
+# Security
+VENDOR_SECURITY_PATCH := 2024-05-01
+
+# inherit from the proprietary version
+include vendor/motorola/dubai/BoardConfigVendor.mk
